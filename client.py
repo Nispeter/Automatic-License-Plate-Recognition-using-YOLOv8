1dbcbec4cdfb2f6ec2aea27bf0199286a6271257
Nispeter <64810836+Nispeter@users.noreply.github.com>
2023-10-22 16:50:29 -0300

    dockerising and port to server

---
+++ client.py
@@ -0,0 +1,12 @@
+import requests
+
+# Endpoint
+url = "http://127.0.0.1:5000/detect_license_plate"
+
+# Loop to send 6 placeholder images
+for i in range(6):
+    image_filename = f"sample_frame_{i}.jpg"  # Adjust this to point to your actual images
+    with open(image_filename, 'rb') as image_file:
+        response = requests.post(url, files={"image": image_file})
+    
+    print(f"Sent image {i+1}. Server response:", response.json())
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from model_constants import FRAME_WINDOW
 from ultralytics import YOLO
+from flask import Flask, request, jsonify
+from threading import Thread
 import cv2
 
 import util
@@ -6,26 +9,16 @@ from sort.sort import *
 from util import get_car, read_license_plate, write_csv
 
 
-results = {}
 
-mot_tracker = Sort()
+def process_frames(frames):
+    results = {}
+    frame_nmr = -1
+    mot_tracker = Sort()
 
-# load models
-coco_model = YOLO('yolov8n.pt')
-license_plate_detector = YOLO('license_plate_detector.pt')
+    vehicles = [2, 3, 5, 7]
 
-# load video
-cap = cv2.VideoCapture('./sample.mp4')
-
-vehicles = [2, 3, 5, 7]
-
-# read frames
-frame_nmr = -1
-ret = True
-while ret:
-    frame_nmr += 1
-    ret, frame = cap.read()
-    if ret:
+    for frame in frames:
+        frame_nmr += 1
         results[frame_nmr] = {}
         # detect vehicles
         detections = coco_model(frame)[0]
@@ -47,7 +40,6 @@ while ret:
             xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate, track_ids)
 
             if car_id != -1:
-
                 # crop license plate
                 license_plate_crop = frame[int(y1):int(y2), int(x1): int(x2), :]
 
@@ -59,11 +51,77 @@ while ret:
                 license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
 
                 if license_plate_text is not None:
-                    results[frame_nmr][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
-                                                  'license_plate': {'bbox': [x1, y1, x2, y2],
-                                                                    'text': license_plate_text,
-                                                                    'bbox_score': score,
-                                                                    'text_score': license_plate_text_score}}
+                    results[frame_nmr][car_id] = {
+                        'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
+                        'license_plate': {
+                            'bbox': [x1, y1, x2, y2],
+                            'text': license_plate_text,
+                            'bbox_score': score,
+                            'text_score': license_plate_text_score
+                        }
+                    }
 
-# write results
-write_csv(results, './test.csv')
+    # write results
+    write_csv(results, './test.csv')
+    return results
+
+
+app = Flask(__name__)
+# Frame buffer
+frame_buffer = []
+
+@app.route('/detect_license_plate', methods=['POST'])
+def detect_license_plate():
+    image_file = request.files['image'].read()
+    np_image = np.frombuffer(image_file, np.uint8)
+    frame = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
+
+    # Add the frame to the buffer
+    frame_buffer.append(frame)
+
+    response_data = []
+
+    # Check if buffer has 5 frames
+    if len(frame_buffer) == FRAME_WINDOW:
+        # Use the process_frames function
+        results = process_frames(frame_buffer)
+
+        # Clear the buffer after processing
+        frame_buffer.clear()
+
+        # Process the results to prepare the response for each frame
+        for idx in range(FRAME_WINDOW):
+            frame_data = results.get(idx, {})
+            for car_id, data in frame_data.items():
+                license_plate_data = data.get('license_plate', {})
+                license_plate_text = license_plate_data.get('text', None)
+                license_plate_text_score = license_plate_data.get('text_score', None)
+
+                if license_plate_text:
+                    response_data.append({
+                        'frame_number': idx,
+                        'car_id': car_id,
+                        'license_plate_text': license_plate_text,
+                        'probability': license_plate_text_score
+                    })
+
+    return jsonify(response_data)
+
+
+
+def listen_continuously():
+    while True:
+        print("waiting...")
+        time.sleep(2)
+
+if __name__ == '__main__':
+    # Load models
+    coco_model = YOLO('yolov8n.pt')
+    license_plate_detector = YOLO('license_plate_detector.pt')
+    
+    # Start the continuous listening function in a separate thread
+    listen_thread = Thread(target=listen_continuously)
+    listen_thread.start()
+
+    # Run Flask server
+    app.run(debug=True, port=5000)
